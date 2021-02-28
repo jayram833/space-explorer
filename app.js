27448961f2deff1da4e11f80d31c841152891b23
jpansare <jaypansare96@gmail.com>
2021-02-28 22:06:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// const getData = async function () {
+//   const response = await fetch(
+//     `https://api.nasa.gov/planetary/apod?api_key=fQOSbqi5qRX4mfdYZdAhmaUzLIgXcUsttYeNeoaQ`
+//   );
+//   const data = await response.json();
+//   console.log(data);
+//   document.querySelector(".title").innerHTML = data.title;
+//   document.querySelector(".date").innerHTML = data.date;
+//   document.querySelector(".news").innerHTML = data.explanation;
+//   document.querySelector(".hdimage").src = data.url;
+// };
+
+// getData();
