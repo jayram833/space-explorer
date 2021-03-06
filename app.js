47df179560e97cb818ae992281fc17f2dc663c08
jpansare <jaypansare96@gmail.com>
2021-03-06 13:23:15 +0530

    footer

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const getData = async function () {
-  const response = await fetch(
-    `https://api.nasa.gov/planetary/apod?api_key=fQOSbqi5qRX4mfdYZdAhmaUzLIgXcUsttYeNeoaQ`
-  );
-  const data = await response.json();
-  console.log(data);
-  document.querySelector(".hdimage").src = data.url;
+  try {
+    const response = await fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=fQOSbqi5qRX4mfdYZdAhmaUzLIgXcUsttYeNeoaQ`
+    );
+    const data = await response.json();
+    console.log(data);
+    document.querySelector(".hdimage").src = data.url;
 
-  document.querySelector(".news").innerHTML = data.explanation;
-  document.querySelector(".ntitle").innerHTML = data.title;
-  document.querySelector(".ndate").innerHTML = data.date;
-  document.querySelector(".copyrights").innerHTML = data.copyright;
+    document.querySelector(".news").innerHTML = data.explanation;
+    document.querySelector(".ntitle").innerHTML = data.title;
+    document.querySelector(".ndate").innerHTML = data.date;
+    if (data.copyright == undefined) {
+      document.querySelector(".copyrights").innerHTML = "No Author";
+    } else {
+      document.querySelector(".copyrights").innerHTML = data.copyright;
+    }
+  } catch (err) {
+    alert("failed to fetch data");
+  }
 };
 
 getData();
