fe6c45ba318014e044c3de24e1f71653a7bea340
jpansare <jaypansare96@gmail.com>
2021-03-07 20:26:43 +0530

    api changes

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ const getData = async function () {
     );
     const data = await response.json();
     console.log(data);
-    document.querySelector(".hdimage").src = data.url;
 
-    document.querySelector(".news").innerHTML = data.explanation;
-    document.querySelector(".ntitle").innerHTML = data.title;
-    document.querySelector(".ndate").innerHTML = data.date;
-    if (data.copyright == undefined) {
-      document.querySelector(".copyrights").innerHTML = "No Author";
-    } else {
-      document.querySelector(".copyrights").innerHTML = data.copyright;
-    }
+    const { date, explanation, title, url, copyright } = data;
+    console.log(date, explanation, title, url);
+
+    document.querySelector(".hdimage").src = url;
+
+    document.querySelector(".news").innerHTML = explanation;
+    document.querySelector(".ntitle").innerHTML = title;
+    document.querySelector(".ndate").innerHTML = date;
+    if (!copyright) return;
+    else document.querySelector(".copyrights").innerHTML = copyright;
   } catch (err) {
     alert("failed to fetch data");
   }
